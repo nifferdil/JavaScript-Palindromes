606f953a97d53e03f98dcb3268e78a0fd868ceec
Molly Waggett & Jennifer Morkunas <student@epicodus.com>
2015-09-16 14:37:21 -0700

    Write palindrome function using recursion (uglyyyy :[ ).

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,20 @@ var isPalindrome = function(word) {
   return word.split("").reverse().join("") === word;
 };
 
+var isPalindromeRecursive = function(word) {
+  if (word.length === 1) {
+    return true;
+  } else if (word.length === 2) {
+    return word[0] === word[word.length-1];
+  } else {
+    if (word[0] === word[word.length-1]) {
+      return isPalindromeRecursive(word.substring(1, word.length-1));
+    } else {
+      return false;
+    }
+  }
+}
+
 // $(document).ready(function() {
 //
 // });
--- spec/specs.js
+++ spec/specs.js
@@ -7,3 +7,29 @@ describe('isPalindrome', function() {
     expect(isPalindrome("hello")).to.equal(false);
   });
 });
+
+describe('isPalindromeRecursive', function() {
+  it("will detect if a word is the same backward or forward", function() {
+    expect(isPalindromeRecursive("racecar")).to.equal(true);
+  });
+
+  it("will detect if a word is the same backward or forward", function() {
+    expect(isPalindromeRecursive("raceecar")).to.equal(true);
+  });
+
+  it("will detect if a word is the same backward or forward", function() {
+    expect(isPalindromeRecursive("hello")).to.equal(false);
+  });
+
+  it("consider single letter a palindrome", function() {
+    expect(isPalindromeRecursive("a")).to.equal(true);
+  });
+
+  it("accommodates two-letter words", function() {
+    expect(isPalindromeRecursive("aa")).to.equal(true);
+  });
+
+  it("accommodates two-letter words", function() {
+    expect(isPalindromeRecursive("ab")).to.equal(false);
+  });
+});
